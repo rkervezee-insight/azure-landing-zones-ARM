66189a7765304e7999186242edf2476c46303091
tulpy <stephen.tulp@outlook.com>
2021-05-06 16:21:19 +0800

    formatiing updates

--- reference/templates/Monitoring/eventHub/deploy.ps1
+++ reference/templates/Monitoring/eventHub/deploy.ps1
@@ -8,20 +8,19 @@ April 28, 2021 1.0
     - Initial script
 
   .DESCRIPTION
-    This template enables you to deploy a EventHubs Standard namespace, an Event Hub, a consumer group and authorizationRules
+    This template enables you to deploy an Azure Private DNS Zone
 #>
 
 # Parameters
 $templateFile = ".\azuredeploy.json"
-$templateParameterFile = ".\parameters\azuredeploy.parameters.json"
-$rg = "sjt-syd-cor-arg-monitoring"
-$subId = "8f8224ca-1a9c-46d1-9206-1cf2a7c51de8"
+$rg = "rg-auea-pr-prd-splunkIntegration-01"
+$subId = "1902a180-efa2-41dc-b943-b29398a3bb90"
 
 # Select Subscription
 Select-AzSubscription -subscriptionName $subId
 
 # Test the deployment with "What If"
-New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $templateFile -TemplateParameterFile $templateParameterFile
+New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $templateFile -TemplateParameterFile $templateParameterFile -WhatIf
 
 # Do the deployment
-#New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $templateFile -TemplateParameterFile $templateParameterFile
+New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $templateFile -TemplateParameterFile $templateParameterFile
