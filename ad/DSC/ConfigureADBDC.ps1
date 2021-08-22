7d93d9cd5a2a27bc1ba132fd96d799d16ef9094d
tulpy <stephen.tulp@outlook.com>
2021-08-22 13:56:36 +0800

    updates

---
+++ ad/DSC/ConfigureADBDC.ps1
@@ -0,0 +1,61 @@
+configuration ConfigureADBDC
+{
+   param
+    (
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName xActiveDirectory, xPendingReboot
+
+    Node localhost
+    {
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+        
+        xWaitForADDomain DscForestWait
+        {
+            DomainName = $DomainName
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount
+            RetryIntervalSec = $RetryIntervalSec
+        }
+        xADDomainController BDC
+        {
+            DomainName = $DomainName
+            DomainAdministratorCredential = $DomainCreds
+            SafemodeAdministratorPassword = $DomainCreds
+            DatabasePath = "F:\NTDS"
+            LogPath = "F:\NTDS"
+            SysvolPath = "F:\SYSVOL"
+            DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+<#
+        Script UpdateDNSForwarder
+        {
+            SetScript =
+            {
+                Write-Verbose -Verbose "Getting DNS forwarding rule..."
+                $dnsFwdRule = Get-DnsServerForwarder -Verbose
+                if ($dnsFwdRule)
+                {
+                    Write-Verbose -Verbose "Removing DNS forwarding rule"
+                    Remove-DnsServerForwarder -IPAddress $dnsFwdRule.IPAddress -Force -Verbose
+                }
+                Write-Verbose -Verbose "End of UpdateDNSForwarder script..."
+            }
+            GetScript =  { @{} }
+            TestScript = { $false}
+            DependsOn = "[xADDomainController]BDC"
+        }
+#>
+        xPendingReboot RebootAfterPromotion {
+            Name = "RebootAfterDCPromotion"
+            DependsOn = "[xADDomainController]BDC"
+        }
+
+    }
+}
