abbe24c385781d0d334812f24ab836122fa900f8
tulpy <stephen.tulp@outlook.com>
2021-09-09 11:56:50 +0800

    update script

--- reference/barcelona/!scripts/deploy-azureAdDiagnostics.ps1
+++ reference/barcelona/!scripts/deploy-azureAdDiagnostics.ps1
@@ -8,14 +8,14 @@ August 12, 2021 1.0
     - Initial script
 
   .DESCRIPTION
-    This script deploys Azure AD Activity Logs to Log Analytics, use this if you want to only deploy this component only.
+    This script sends Azure AD Activity Logs to a Log Analytics workspace.
 #>
 
 # Parameters
 
 $Location = "australiaeast"
 $DeploymentName = "azureAdActivityLogs"
-$lawResourceId = "/subscriptions/8f8224ca-1a9c-46d1-9206-1cf2a7c51de8/resourcegroups/sjt-syd-cor-arg-management/providers/microsoft.operationalinsights/workspaces/sjt-syd-cor-law-c310db5e"
+$lawResourceId = ""
 
 New-AzTenantDeployment -Name  "$($DeploymentName)-$($Location)" ` `
   -Location $Location `
